2121b91d62e51559dc955bbe02293f1cd7931ac5
Daniel Costa <daniel.c.dan@gmail.com>
2023-07-12 16:46:08 -0300

    add search functions

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 import Todo from './components/Todo';
 import TodoForm from './components/TodoForm';
+import Search from './components/Search';
 
 import "./App.css";
 
@@ -26,6 +27,8 @@ function App() {
     isComplete: false,
   },   
   ]);
+
+  const [search, setSearch] = useState("");
 
   const addTodo = (text, category) => {
     const newTodos = [...todos,
@@ -57,8 +60,9 @@ function App() {
   return (
     <div className='app'>
       <h1>Lista de Tarefa</h1>
+      <Search search={search} setSearch={setSearch}/>
       <div className='todo-list'>
-        {todos.map((todo) => (
+        {todos.filter((todo) => todo.text.toLocaleLowerCase().includes(search.toLocaleLowerCase())).map((todo) => (
           <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
         ))}
       </div>
